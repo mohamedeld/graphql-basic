0c769a292c1c6717c120584959bd7eda8de0c32a
mohamedeld <mohamed.azoz20010@gmail.com>
2023-07-15 15:27:03 +0300

    add CRUD to category

---
+++ data/categoryData.js
@@ -0,0 +1,35 @@
+const categories = [
+  {
+    id: 1,
+    name: "clothes",
+  },
+  {
+    id: 1,
+    name: "clothes",
+  },
+  {
+    id: 7,
+    name: "clothes",
+  },
+  {
+    id: 6,
+    name: "clothes",
+  },
+  {
+    id: 5,
+    name: "clothes",
+  },
+  {
+    id: 4,
+    name: "clothes",
+  },
+  {
+    id: 3,
+    name: "clothes",
+  },
+  {
+    id: 2,
+    name: "clothes",
+  },
+];
+module.exports = categories;
--- graphql/schema.js
+++ graphql/schema.js
@@ -1,18 +1,120 @@
-const { GraphQLObjectType, GraphQLString, GraphQLSchema } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLList,
+  GraphQLID,
+  GraphQLFloat,
+} = require("graphql");
+const productData = require("../data/productData");
+const categories = require("../data/categoryData");
+
+const categoryType = new GraphQLObjectType({
+  name: "categoryType",
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+  },
+});
+
+const productType = new GraphQLObjectType({
+  name: "productType",
+  fields: {
+    sold: { type: GraphQLInt },
+    ratingsQuantity: { type: GraphQLInt },
+    id: { type: GraphQLID },
+    title: { type: GraphQLString },
+    quantity: { type: GraphQLInt },
+    price: { type: GraphQLInt },
+    imageCover: { type: GraphQLString },
+    category: {
+      type: categoryType,
+      resolve: (parent, args) => {
+          console.log(parent);
+        return categories.find((item) => item.id == parent.category);
+      },
+    },
+    ratingsAverage: { type: GraphQLFloat },
+  },
+});
+
 
 
 const itiQuery = new GraphQLObjectType({
-    name: "itiQuery",
-    fields: {
-        hello:{
-            type:GraphQLString,
-            resolve:(parent,args)=>{
-                return "hello world"
-            }            
-        }
-    }
+  name: "itiQuery",
+  fields: {
+    products: {
+      type: new GraphQLList(productType),
+      resolve: (parent, args) => {
+        return productData;
+      },
+    },
+    product: {
+      type: productType,
+      args: { id: { type: GraphQLID } },
+      resolve: (parent, args) => {
+        return productData.find((item) => item.id == args.id);
+      },
+    },
+    categories: {
+      type: new GraphQLList(categoryType),
+      resolve: (parent, args) => {
+        return categories;
+      },
+    },
+  },
 });
 
-module.exports = new GraphQLSchema({
-    query:itiQuery
+const itiMuatation = new GraphQLObjectType({
+    name:'itiMuatation',
+    fields:{
+        addCategory:{
+            type:new GraphQLList(categoryType),
+            args:{
+                id:{type:GraphQLID},
+                name:{type:GraphQLString}
+            },
+            resolve:(parent,args)=>{
+                categories.push(args);
+                return categories
+            }
+        },
+        updateCategory:{
+            type:categoryType,
+            args:{
+                id:{type:GraphQLID},
+                name:{type:GraphQLString}
+            },
+            resolve:(parent,args)=>{
+                const category = categories.find(item => item.id == args.id);
+                if(!category){
+                    throw new Error("category id was not found")
+                }
+                category.name = args.name;
+                return category;
+            }
+        },
+        deleteCategory:{
+            type:categoryType,
+            args:{
+                id:{type:GraphQLID}
+            },
+            resolve:(parent,args)=>{
+                const categoryIndex = categories.findIndex(
+                  (category) => category.id === args.id
+                );
+                if (categoryIndex === -1) {
+                  throw new Error("Category not found");
+                }
+                const deleteCategory = categories.splice(categoryIndex, 1)[0];
+                return deleteCategory;
+            }
+        }
+    }
 })
+
+module.exports = new GraphQLSchema({
+  query: itiQuery,
+  mutation: itiMuatation,
+});
